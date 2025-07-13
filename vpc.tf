b11f1530866d454076561c5644a495347df583bd
anusha662 <anushamanthurthi666@gmail.com>
2025-07-13 22:30:46 +0530

    create web security

--- vpc.tf
+++ vpc.tf
@@ -210,4 +210,39 @@ resource "aws_network_acl_association" "crm-db-nacl-asc" {
 }
 
 
+# crm web security group
+resource "aws_security_group" "crm-web-sg" {
+  name        ="crm-web-sg"
+  description = "Allow SSH & HTTP traffic"
+  vpc_id      = aws_vpc.crm-vpc.id
+
+  tags = {
+    Name = "crm-web-sg"
+  }
+}
+ # crm web security group -ingress
+
+resource "aws_vpc_security_group_ingress_rule" "crm-web-sg-ingress-ssh" {
+  security_group_id = aws_security_group.crm-web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "crm-web-sg-ingress-http" {
+  security_group_id = aws_security_group.crm-web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# crm web security group -egress
+
+resource "aws_vpc_security_group_egress_rule" "crm-web-sg-egress" {
+  security_group_id = aws_security_group.crm-web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
 
