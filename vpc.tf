8d75ead6b8f506d69497808af6fe603079c5a4d9
anusha662 <anushamanthurthi666@gmail.com>
2025-07-13 19:14:23 +0530

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,39 @@ resource "aws_vpc" "crm-vpc" {
     Name = "crm-vpc"
   }
 }
+
+
+# web subnet
+
+resource "aws_subnet" "crm-web-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch="true"
+  tags = {
+    Name = "crm-web-subnet"
+  }
+}
+
+# api subnet
+
+resource "aws_subnet" "crm-api-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch="true"
+
+
+  tags = {
+    Name = "crm-api-subnet"
+  }
+}
+
+# database subnet
+
+resource "aws_subnet" "crm-db-si" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "crm-database-subnet"
+  }
+}
